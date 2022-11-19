525d431e1e953fa4564e68205c4dcc9a8f50d0c9
Hanna Cui <ge65tey@mytum.de>
2022-11-19 21:09:41 +0100

    added commit to the post services

--- server/server.py
+++ server/server.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, request
 import sqlite3
+import csv
 
 #UPLOAD_FOLDER = './images'
 #ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
-
 
 
 
@@ -13,29 +12,33 @@ app = Flask(__name__)
 
 con = sqlite3.connect("app.db", check_same_thread=False)
 
+
+
+
 # hello beautiful world
 @app.route("/")
 def hello_app():
     return "Hello World"
 
-# App first-time registration of a user, args user, longitude, latitude, avatar
+# App first-time registration of a user, args: user (longitude, latitude, avatar hardcoded first)
 # returns Success if inserted to db, Failure if username not unique
 @app.route("/register", methods=['POST'])
 def register():
     userId = request.args['user']
-    longitude = request.args['longitude']
-    latitude = request.args['latitude']
-    avatarId = request.args['avatar']
+    # longitude = request.args['longitude']
+    # latitude = request.args['latitude']
+    # avatarId = request.args['avatar']
 
     cur = con.cursor()
 
     try:
-        cur.execute("INSERT INTO users VALUES (?,0,?,?,?)", (userId, longitude, latitude, avatarId))
+        cur.execute("INSERT INTO users VALUES (?,0,null,null,0)", (userId, ))
+        con.commit()
         cur.close()
-        return "Success"
+        return jsonify(True)
     except sqlite3.IntegrityError:
         cur.close()
-        return "Failure"
+        return jsonify(False)
 
 
 
@@ -50,7 +53,7 @@ def leaderboard():
 
 
 
-# App POSTS args image, user, issue
+# App POSTS args: image, user, issue
 # returns [result, updatedScore]
 @app.route("/upload_Issue", methods=['POST'])
 def upload():
@@ -82,7 +85,8 @@ def upload():
      INSERT INTO fixedIssues VALUES
          (?, ?, ?, ?) """, (issueId, issueLon, issueLat, userId, ))
     # remove from openIssues
-    cur.execute("DELETE FROM openIssues WHERE issueId = ?", (issueId, ))     
+    cur.execute("DELETE FROM openIssues WHERE issueId = ?", (issueId, ))  
+    con.commit()   
     cur.close()
 
 
@@ -90,7 +94,7 @@ def upload():
 
     
 
-# App POSTS args user, longitude, latitude
+# App POSTS args: user, longitude, latitude
 # returns Success 
 @app.route("/update_Position", methods=['POST'])
 def updatePos():
@@ -101,6 +105,7 @@ def updatePos():
     cur = con.cursor()
     # does null / None for longitude/latitude work ?
     cur.execute("UPDATE users SET longitude = ?, latitude = ? WHERE userId = ?", (longitude, latitude, userId))
+    con.commit()
     cur.close()
 
     # return nothing ? 
@@ -121,7 +126,7 @@ def others():
 
 
 # App GETS the openIssues to render on map
-# returns all [[openIssue]]
+# returns all [[issueId, longitude, latitude, radius, showLongitude, showLatitude]]
 @app.route("/get_openIssues", methods=['GET'])
 def openIssues():
     cur = con.cursor()
@@ -132,7 +137,7 @@ def openIssues():
 
 
 # App GETS the fixedIssues to render already fixed issues on map with their fixer
-# returns all [[fixedIssue]]
+# returns all [[issueId, longitude, latitude, fixedBy]]
 @app.route("/get_fixedIssues", methods=['GET'])
 def closedIssues():
     cur = con.cursor()
