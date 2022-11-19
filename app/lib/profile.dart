c33404d3ef21b81822cf1e9d657ceefb636435ad
Woojin Ko <woojin.ko@tum.de>
2022-11-19 17:09:29 +0100

    map doing

--- app/lib/profile.dart
+++ app/lib/profile.dart
@@ -41,66 +41,60 @@ class _ProfileState extends State<Profile> {
               physics: const BouncingScrollPhysics(
                 parent: AlwaysScrollableScrollPhysics(),
               ),
-              child: InkWell(
-                onTap: () => {},
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 1.2,
-                  padding: const EdgeInsets.only(top: 20),
-                  child: Column(
-                    children: [
-                      const Center(
-                        child: ClipOval(
-                            child: Image(
-                                width: 200,
-                                image: AssetImage(
-                                    'assets/avatar/sleepingsloth.jpg'))),
-                      ),
-                      const SizedBox(height: 50),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: Container(
-                          height: MediaQuery.of(context).size.width * 0.15,
-                          width: MediaQuery.of(context).size.width * 0.75,
-                          decoration: const BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.0)),
-                            gradient: LinearGradient(
-                              colors: [Color(0xFF18E299), Color(0xFF0FFAE6)],
-                              begin: Alignment(-1.0, -2.0),
-                              end: Alignment(1.0, 2.0),
-                            ),
-                          ),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    width: 12.0,
-                                    child: const Icon(Icons.person),
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  const Padding(
-                                    padding:
-                                        EdgeInsets.symmetric(vertical: 18.0),
-                                    child: Text(
-                                      'Ava',
-                                      style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w600,
-                                          color: Colors.white),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
+              child: Container(
+                height: MediaQuery.of(context).size.height / 1.2,
+                padding: const EdgeInsets.only(top: 20),
+                child: Column(
+                  children: [
+                    const Center(
+                      child: ClipOval(
+                          child: Image(
+                              width: 200,
+                              image: AssetImage('assets/avatar/sloth.jpg'))),
+                    ),
+                    const SizedBox(height: 20),
+                    InkWell(
+                      onTap: () => {},
+                      child: Container(
+                        height: MediaQuery.of(context).size.width * 0.15,
+                        width: MediaQuery.of(context).size.width * 0.55,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                          gradient: LinearGradient(
+                            colors: [Color(0xAF18E299), Color(0xAF0FFAE6)],
+                            begin: Alignment(-1.0, -2.0),
+                            end: Alignment(1.0, 2.0),
                           ),
                         ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 12.0,
+                                  child: const Icon(Icons.person),
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 18.0),
+                                  child: Text(
+                                    'My Avatar',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
