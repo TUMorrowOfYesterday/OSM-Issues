f6de6b485ce557ae21c93a559b1831a08da029e9
Joong-Won Seo <Joong-Won.Seo@tum.de>
2022-11-20 10:00:20 +0100

    asd

--- app/lib/globals.dart
+++ app/lib/globals.dart
@@ -2,5 +2,5 @@ library app.globals;
 
 int avatarId = 1;
 String userId = '';
-String serverUrl = 'https://04dced36120961.lhr.life/';
+String serverUrl = 'https://da148598d5818c.lhr.life/';
 int currentChallengeId = -1;
